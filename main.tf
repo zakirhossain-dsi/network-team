fce83f0a84bd391ef6387435d60b7cb775cf6264
Zakir <zakir.mbstu.ict07@gmail.com>
2026-01-18 15:11:05 +0800

    Added main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+variable "public_ips" {
+  type = list(string)
+  default = ["8.8.8.8", "1.1.1.1"]
+}
+
+output "public_ips" {
+  value = var.public_ips
+  description = "All public IP addresses are defined in this workspace"
+}
